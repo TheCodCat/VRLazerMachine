b2355ba88e02d7375b41ffeb02c1a6f6cf4b3987
TheCodCat <166803529+TheCodCat@users.noreply.github.com>
2025-11-28 16:23:59 +0400

    123

---
+++ Assets/Scripts/lazer_mashine/ExtrudeController.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ExtrudeController : MonoBehaviour
+{
+    [SerializeField] private Transform extrudePoint;
+
+
+}
--- Assets/Scripts/lazer_mashine/LazerData.cs
+++ Assets/Scripts/lazer_mashine/LazerData.cs
@@ -8,9 +8,12 @@ public class LazerData : ScriptableObject
     [SerializeField] private string triggerName;
     [SerializeField] private string previewNameBool;
     [SerializeField] private TypeLazer typeLazer;
+    [Space]
+    [SerializeField] private Texture2D decal;
 
     public string Description { get => description; }
     public string TriggerName { get => triggerName; }
     public string PreviewName { get => previewNameBool; }
     public TypeLazer TypeLazer { get => typeLazer; }
+    public Texture2D Decal { get => decal; }
 }
